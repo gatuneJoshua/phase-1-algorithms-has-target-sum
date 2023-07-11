6a5baaa830c61b760b3176fc98f1ff2762b05b15
Joshua <joshua.gatune@student.moringaschool.com>
2023-07-11 10:47:20 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seen = new Set();
+  for (let num of array) {
+    if (seen.has(target - num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+   Time Complexity: O(n)
+  The function iterates through the array once, where n is the length of the array.
+  The operations performed inside the loop, such as set lookups and additions, have an average time complexity of O(1) due to the use of a set.
 */
 
 // You can run `node index.js` to view these console logs
